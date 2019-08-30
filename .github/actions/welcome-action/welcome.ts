5f953fe645699b6d2e49d1277d25441f7d52af85
Jeff Rafter <jeffrafter@gmail.com>
2019-08-30 12:53:13 -0700

    Update welcome.ts

--- .github/actions/welcome-action/welcome.ts
+++ .github/actions/welcome-action/welcome.ts
@@ -8,13 +8,6 @@ async function run() {
     core.debug(msg);
     core.setOutput("message", msg);
 
-    // const issue: { owner: string; repo: string; number: number } =
-    //   github.context.issue;
-    // if (github.context.payload.action !== "opened") {
-    //   console.log("No issue or pull request was opened, skipping");
-    //   return;
-    // }
-
     // https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret
     const client: github.GitHub = new github.GitHub(
       process.env["GITHUB_TOKEN"] || ""
@@ -22,13 +15,59 @@ async function run() {
 
     const nwo = process.env["GITHUB_REPOSITORY"] || "/";
     const [owner, repo] = nwo.split("/");
+    const suffix = "";
+    const branchName = `submission${suffix}`;
+    const ref = `refs/heads/${branchName}`;
+    const sha = process.env["GITHUB_SHA"] || "";
+
+    let branch = await client.git.getRef({
+      owner,
+      repo,
+      ref
+    });
+    if (branch) {
+      throw new Error(`Branch ${ref} already exists`);
+    }
 
     // https://octokit.github.io/rest.js/#octokit-routes-issues
     await client.issues.create({
       owner: owner,
       repo: repo,
-      title: "Welcome",
+      title: "Welcome" + suffix,
       body: "Thanks for using this template. To get started..."
+    });
+
+    // Create a branch submission
+    // https://octokit.github.io/rest.js/#octokit-routes-git-create-ref
+    await client.git.createRef({
+      owner,
+      repo,
+      ref,
+      sha
+    });
+
+    // Protect that branch
+    // https://octokit.github.io/rest.js/#octokit-routes-repos-update-branch-protection
+    await client.repos.updateBranchProtection({
+      owner: owner,
+      repo: repo,
+      branch: branchName,
+      required_status_checks: null,
+      enforce_admins: true,
+      required_pull_request_reviews: null,
+      restrictions: null
+    });
+
+    // Create a pull request from master to that branch
+    // https://octokit.github.io/rest.js/#octokit-routes-pulls
+    // Could be a draft, then marking ready-for-review marks as submitted
+    await client.pulls.create({
+      owner,
+      repo,
+      title: "Submission" + suffix,
+      body: "This is the submission",
+      head: "ref/heads/master", // master is what the is committed to
+      base: ref // The submission branch is the base
     });
   } catch (error) {
     core.setFailed(error.message);
