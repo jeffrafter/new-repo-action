718a849132bac85e669047d5514245fab55b7d80
Jeff Rafter <jeffrafter@gmail.com>
2019-08-30 04:28:07 -0700

    Make it create an issue

--- .github/actions/welcome-action/welcome.ts
+++ .github/actions/welcome-action/welcome.ts
@@ -1,4 +1,5 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
   try {
@@ -6,6 +7,29 @@ async function run() {
     const msg = `Welcome from ${welcomeInput}!`;
     core.debug(msg);
     core.setOutput("message", msg);
+
+    // const issue: { owner: string; repo: string; number: number } =
+    //   github.context.issue;
+    // if (github.context.payload.action !== "opened") {
+    //   console.log("No issue or pull request was opened, skipping");
+    //   return;
+    // }
+
+    // https://help.github.com/en/articles/virtual-environments-for-github-actions#github_token-secret
+    const client: github.GitHub = new github.GitHub(
+      process.env["GITHUB_TOKEN"]
+    );
+
+    const nwo = process.env["GITHUB_REPOSITORY"] || "/";
+    const [owner, repo] = nwo.split("/");
+
+    // https://octokit.github.io/rest.js/#octokit-routes-issues
+    await client.issues.createIssue({
+      owner: owner,
+      repo: repo,
+      title: "Welcome",
+      body: "Thanks for using this template. To get started..."
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
